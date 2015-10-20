2f166fa1af1c0fc5369eb8d3687f218e62b44644
Roman Levin <romanlevin@gmail.com>
2015-10-19 21:57:51 -0700

    [ex17] Fix strncpy overflow bug

--- ex17.c
+++ ex17.c
@@ -104,9 +104,11 @@ void Database_set(struct Connection *conn, int id, const char *name, const char 
         addr->set = 1;
         //WARNING
         char *res = strncpy(addr->name, name, MAX_DATA);
+        addr->name[MAX_DATA - 1] = '\0';
         if(!res) die("Name copy failed");
 
         res = strncpy(addr->email, email, MAX_DATA);
+        addr->email[MAX_DATA - 1] = '\0';
         if(!res) die("Email copy failed");
 }
 
